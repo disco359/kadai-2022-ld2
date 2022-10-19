b6eebc872a6ac19f8e3a7031140e64d03e347a9a
disco359 <ji5.arisk@gmail.com>
2022-10-19 05:35:12 +0000

    kadai2

---
+++ kadai-02/index.js
@@ -0,0 +1,9 @@
+const firstName ="taro"
+firstname =
+const familyName ="yamada"
+
+const fullName = firstNname + familyName
+console.log(fullName)
+
+
+const num = 1 + 2+ 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 
