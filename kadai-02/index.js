d5f1066c3c3bb43cf1cf4c625b91e5f7a5ae895e
disco359 <ji5.arisk@gmail.com>
2022-10-26 03:02:50 +0000

    課題2を追加

--- kadai-02/index.js
+++ kadai-02/index.js
@@ -1,9 +1,17 @@
 const firstName ="taro"
-firstname =
 const familyName ="yamada"
 
-const fullName = firstNname + familyName
+const fullName = firstName + familyName
 console.log(fullName)
 
 
-const num = 1 + 2+ 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 
+const num = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+console.log(num)
+
+const dog = false
+const cat = true
+
+console.log("犬または猫",dog || cat)
+console.log("犬と猫の両方",dog && cat)
+console.log("犬も猫もnot1",!dog && !cat)
+console.log("犬も猫もnot2",!(dog || cat))
